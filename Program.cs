ad41afa2bcea1a46d16341a95a874e0e5c473a03
ziril <ziril@DESKTOP-19E4QUV>
2025-05-19 19:58:31 +0200

    Unterscheiden zweischen Serien und Filmen

--- Program.cs
+++ Program.cs
@@ -5,6 +5,59 @@ using System.Xml.Linq;
 
 public class WebPageLinkExtractor
 {
+    // Anzahl der Staffeln ermitteln
+    static int AnzStaffeln(string basislinkderserie)
+    {
+        // Unterseite öffnen
+        string urlinner = basislinkderserie;
+        var htmlDocinner = new HtmlDocument();
+
+        try
+        {
+            HtmlWeb webinner = new HtmlWeb();
+            htmlDocinner = webinner.Load(urlinner);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler beim Laden der Webseite: {ex.Message}");
+        }
+        // Anzahl der Staffeln des Anime zählen
+        var anz = htmlDocinner.DocumentNode.SelectNodes("//a")?
+            .Where(node => node.Attributes.Contains("title") && node.Attributes["title"].Value.Contains("Staffel") && !node.Attributes["title"].Value.Contains("Episode"))
+            .Select(node => node.Attributes["title"].Value)
+            .ToList();
+
+        int count = anz.Count;
+        int staffeln = count;
+        return count;
+
+    }
+
+    static int AnzFolgen(string basislinkderserie)
+    {
+        // Unterseite öffnen
+        string urlinner = basislinkderserie;
+        var htmlDocinner = new HtmlDocument();
+
+        try
+        {
+            HtmlWeb webinner = new HtmlWeb();
+            htmlDocinner = webinner.Load(urlinner);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Fehler beim Laden der Webseite: {ex.Message}");
+        }
+        var srcs = htmlDocinner.DocumentNode.SelectNodes("//img")?
+            .Where(img => img.Attributes.Contains("src") && img.Attributes["src"].Value.StartsWith("/public/img/german.svg"))
+            .Select(img => img.Attributes["src"].Value)
+            .ToList();
+
+        int count = srcs.Count;
+        int folgen = count;
+        return count;
+    }
+
     public static void Main(string[] args)
     {
         // Übersichtsseite von Aniworld mit allen Serien und Filmen
@@ -25,7 +78,7 @@ public class WebPageLinkExtractor
             return;
         }
 
-        // Äußere Schleife 1111
+        // Äußere Schleife
         // Alle Links die mit /anime/stream/ anfangen aus dem HTML-Code extrahieren
         var links = htmlDoc.DocumentNode.SelectNodes("//a")?
             .Where(node => node.Attributes.Contains("href") && node.Attributes["href"].Value.StartsWith("/anime/stream/"))
@@ -61,89 +114,115 @@ public class WebPageLinkExtractor
 
             // Innere Schleife
             // Pro Link, diesen öffnen und nach einem Deutsch sprachigem Link suchen
-            foreach (string link in links)
+            if (links != null)
             {
-                // Anzeige bei welchem Link er gerade ist (gekürzt auf max. 16 Stellen
-                //if (link.Length < 30) { Console.WriteLine(link.Substring(14)); } else { Console.WriteLine(link.Substring(14, 16)); }
+                foreach (string link in links)
+                {
+                    // Anzeige bei welchem Link er gerade ist (gekürzt auf max. 16 Stellen
+                    //if (link.Length < 30) { Console.WriteLine(link.Substring(14)); } else { Console.WriteLine(link.Substring(14, 16)); }
 
-                // Wenn im Titel "Stream anschauen" enthalten ist, diesen entfernen
-                if (titles[i].Contains(" Stream anschauen")) { titles[i] = titles[i].Replace(" Stream anschauen", ""); }
+                    // Wenn im Titel "Stream anschauen" enthalten ist, diesen entfernen
+                    if (titles != null) { if (titles[i].Contains(" Stream anschauen")) { titles[i] = titles[i].Replace(" Stream anschauen", ""); } }
 
-                // Ladebalken anzeigen
-                Console.SetCursorPosition(0, 0);
-                Console.WriteLine("┌────────┬─────────────────────────────────────────────────────────────────────────────────────────────────────┐");
-                Console.WriteLine("│        │                                                                                                     │");
-                Console.WriteLine("└────────┴─────────────────────────────────────────────────────────────────────────────────────────────────────┘");
-                Console.SetCursorPosition(10, 1);
-                for (int p = 0; p < progress; p++)
+                    // Ladebalken anzeigen
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine("┌────────┬─────────────────────────────────────────────────────────────────────────────────────────────────────┐");
+                    Console.WriteLine("│        │                                                                                                     │");
+                    Console.WriteLine("└────────┴─────────────────────────────────────────────────────────────────────────────────────────────────────┘");
+                    Console.SetCursorPosition(10, 1);
+                    for (int p = 0; p < progress; p++)
                     {
                         Console.Write("▓");
                     }
 
-                    if ( progress > 0 && progress < totalSteps ) { Console.Write("▒"); } 
-                    else if ( progress == 0 ) { Console.Write("░"); } 
-                    else if ( progress == totalSteps ) { Console.Write("▓"); }
+                    if (progress > 0 && progress < totalSteps) { Console.Write("▒"); }
+                    else if (progress == 0) { Console.Write("░"); }
+                    else if (progress == totalSteps) { Console.Write("▓"); }
                     for (int p = progress; p < totalSteps; p++)
 
                     {
                         Console.Write("░");
                     }
-                Console.SetCursorPosition(1, 1);
+                    Console.SetCursorPosition(1, 1);
                     if (progress < 10)
-                        {
-                            Console.Write("[   " + progress + "% ]");
-                        }
+                    {
+                        Console.Write("[   " + progress + "% ]");
+                    }
                     else if (progress > 9 && progress < 100)
-                        {
-                            Console.Write("[  " + progress + "% ]");
-                        }
+                    {
+                        Console.Write("[  " + progress + "% ]");
+                    }
                     else if (progress > 99)
-                        {
-                            Console.Write("[ " + progress + "% ]");
-                        }
-                Console.SetCursorPosition(0, 3);
+                    {
+                        Console.Write("[ " + progress + "% ]");
+                    }
+                    Console.SetCursorPosition(0, 3);
                     // Fortschritt erhöhen
-                    progress = (int)(Math.Round(100 * j / titles.Count));
+                    if (titles != null) { progress = (int)(Math.Round(100 * j / titles.Count)); }
 
-                // Unterseite öffnen
-                string urlinner = "https://aniworld.to" + link;
-                var htmlDocinner = new HtmlDocument();
+                    // Unterseite öffnen
+                    string urlinner = "https://aniworld.to" + link;
+                    var htmlDocinner = new HtmlDocument();
 
-                try
-                {
-                    HtmlWeb webinner = new HtmlWeb();
-                    htmlDocinner = webinner.Load(urlinner);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Fehler beim Laden der Webseite: {ex.Message}");
-                    return;
-                }
-
-                // Alle img tags auslesen aus einer bestimmten Anime Serie
-                var srcs = htmlDocinner.DocumentNode.SelectNodes("//img")?
-                    .Where(img => img.Attributes.Contains("src") && img.Attributes["src"].Value.StartsWith("/public/img/german.svg"))
-                    .Select(img => img.Attributes["src"].Value)
-                    .ToList();
-
-
-                if (srcs.Count > 0)
-                {
-                    // Linkname : Anzahl der deutschen Folgen in Linksammlung.html abspeichern
-                    // Unterschiedung von "Folgen" und "Folge"
-                    if (srcs.Count == 1)
+                    try
                     {
-                        writer.WriteLine($"<li><a style=\"text-decoration: none; \" href=\"https://aniworld.to{link}\">{titles[i]}</a>:&nbsp;{srcs.Count}&nbsp;Folge</li>");
+                        HtmlWeb webinner = new HtmlWeb();
+                        htmlDocinner = webinner.Load(urlinner);
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        writer.WriteLine($"<li><a style=\"text-decoration: none; \" href=\"https://aniworld.to{link}\">{titles[i]}</a>:&nbsp;{srcs.Count} &nbsp;Folgen</li>");
+                        Console.WriteLine($"Fehler beim Laden der Webseite: {ex.Message}");
+                        return;
                     }
+
+                    // Alle img tags auslesen aus einer bestimmten Anime Serie
+                    //var srcs = htmlDocinner.DocumentNode.SelectNodes("//img")?
+                    //    .Where(img => img.Attributes.Contains("src") && img.Attributes["src"].Value.StartsWith("/public/img/german.svg"))
+                    //    .Select(img => img.Attributes["src"].Value)
+                    //    .ToList();
+
+
+                    //Anzahl der Staffeln des Anime zählen
+                    //var anz = htmlDocinner.DocumentNode.SelectNodes("//a")?
+                    //    .Where(node => node.Attributes.Contains("title") && node.Attributes["title"].Value.Contains("Staffel") && !node.Attributes["title"].Value.Contains("Episode"))
+                    //    .Select(node => node.Attributes["title"].Value)
+                    //    .ToList();
+
+                    // Anzahl der Staffeln bestimmen
+                    int staffeln = AnzStaffeln(urlinner);
+
+                    // Anzahl der Folgen in der Staffel bestimmen
+                    int s = 1;
+                    int summe = 0;
+                    while (s <= staffeln)
+                    {
+                        string urlfolgen = urlinner + "/staffel-" + s;
+                        int folgen = AnzFolgen(urlfolgen);
+                        summe = summe + folgen;
+                        s++;
+                    }
+
+                    if (titles != null)
+                    {
+                        if (summe > 0)
+                        {
+                            // Linkname : Anzahl der deutschen Folgen in Linksammlung.html abspeichern
+                            // Unterschiedung von "Folgen" und "Folge"
+                            if (summe == 1)
+                            {
+                                writer.WriteLine($"<li><a style=\"text-decoration: none; \" href=\"https://aniworld.to{link}\">{titles[i]}</a>:&nbsp;{summe}&nbsp;Film</li>");
+                            }
+                            else
+                            {
+                                writer.WriteLine($"<li><a style=\"text-decoration: none; \" href=\"https://aniworld.to{link}\">{titles[i]}</a>:&nbsp;{summe} &nbsp;Folgen</li>");
+                            }
+                        }
+                    }
+                    // i als Variable für die Title in Titles / j als Variable (double) zum Berechnen des Fortschrittes
+                    i++;
+                    j++;
+
                 }
-                // i als Variable für die Title in Titles / j als Variable (double) zum Berechnen des Fortschrittes
-                i++;
-                j++;
-                
             }
             // Fuß der Linksammler.html Datei schreiben
             writer.WriteLine("  </ul></div></body>\r\n</html>");
